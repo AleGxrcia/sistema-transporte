e5882d3c884ad47f10f16ee5c4d70f167f1e4201
AleGxrcia <fagarciatavarez@gmail.com>
2026-06-04 07:53:31 -0400

    feat(shared): add AggregateRoot with domain event collection

---
+++ backend/src/TransportSystem.Core.Domain/Common/AggregateRoot.cs
@@ -0,0 +1,25 @@
+﻿namespace TransportSystem.Core.Domain.Common
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        protected AggregateRoot() { }
+
+        protected AggregateRoot(TId id) : base(id) 
+        {
+        }
+
+        protected void RaiseDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+    }
+}
