ce1e6fbf1f2f1207ec22d60d7006b009f66e8799
AleGxrcia <fagarciatavarez@gmail.com>
2026-06-05 08:03:47 -0400

    feat(vehicles): add VehicleCapacity value object

---
+++ backend/src/TransportSystem.Core.Domain/Fleet/Aggregates/Vehicle/VehicleCapacity.cs
@@ -0,0 +1,29 @@
+﻿using TransportSystem.Core.Domain.Common;
+
+namespace TransportSystem.Core.Domain.Fleet.Aggregates.Vehicle
+{
+    public class VehicleCapacity : ValueObject
+    {
+        public int Passengers { get; }
+
+        public VehicleCapacity(int passengers)
+        {
+            Passengers = passengers;
+        }
+
+        public static VehicleCapacity Create(int passengers)
+        {
+            if (passengers <= 0 || passengers > 100)
+                throw new DomainException("CAPACITY_INVALID", "La capacidad debe ser entre 1 y 100 pasajeros.");
+
+            return new VehicleCapacity(passengers);
+        }
+
+        public bool CanAccommodate(int requiredPassengers) => Passengers >= requiredPassengers;
+
+        protected override IEnumerable<object?> GetEqualityComponents()
+        {
+            yield return Passengers;
+        }
+    }
+}
