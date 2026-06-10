7faee6fecd8f011425b98e847497ad3fc03362f3
AleGxrcia <fagarciatavarez@gmail.com>
2026-06-10 15:49:26 -0400

    feat(auth): add account service interface and related DTOs for user management

---
+++ backend/src/TransportSystem.Core.Application/Dtos/Auth/CreateUserRequest.cs
@@ -0,0 +1,13 @@
+﻿using TransportSystem.Core.Application.Common.Enums;
+
+namespace TransportSystem.Core.Application.Dtos.Auth
+{
+    public record CreateUserRequest(
+        string Email,
+        string FirstName,
+        string LastName,
+        string Password,
+        UserRole Role,
+        string Origin
+    );
+}
---
+++ backend/src/TransportSystem.Core.Application/Dtos/Auth/UserResult.cs
@@ -0,0 +1,13 @@
+﻿using TransportSystem.Core.Application.Common.Enums;
+
+namespace TransportSystem.Core.Application.Dtos.Auth
+{
+    public record UserResult(
+        string Id,
+        string Email,
+        string FirstName,
+        string LastName,
+        UserRole Role,
+        bool IsActive
+    );
+}
