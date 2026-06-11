4dd8553e777d018c7bf5e5b90a9996fc5144eee0
AleGxrcia <fagarciatavarez@gmail.com>
2026-06-11 09:19:37 -0400

    feat(email): add EmailRequest DTO for email handling

---
+++ backend/src/TransportSystem.Core.Application/Common/Dtos/EmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TransportSystem.Core.Application.Common.Dtos
+{
+    public record EmailRequest(
+        string To,
+        string Subject,
+        string Body,
+        string? From = null
+    );
+}
