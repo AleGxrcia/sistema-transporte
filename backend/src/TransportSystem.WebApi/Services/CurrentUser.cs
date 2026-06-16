eb0b3e505530b158dad26fa6ea81b15c3e6740e8
AleGxrcia <fagarciatavarez@gmail.com>
2026-06-16 15:21:38 -0400

    feat(auth): implement CurrentUser service for user context management

---
+++ backend/src/TransportSystem.WebApi/Services/CurrentUser.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using TransportSystem.Core.Application.Common.Enums;
+using TransportSystem.Core.Application.Common.Exceptions;
+using TransportSystem.Core.Application.Common.Interfaces;
+
+namespace TransportSystem.WebApi.Services
+{
+    public class CurrentUser : ICurrentUser
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUser(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private ClaimsPrincipal? User => _httpContextAccessor.HttpContext?.User;
+
+        public Guid Id => Guid.Parse(
+            _httpContextAccessor.HttpContext?.User?.FindFirstValue("uid")
+            ?? throw new UnauthorizedException("Usuario no autenticado."));
+
+        public string Email => _httpContextAccessor.HttpContext?.User?
+            .FindFirstValue(ClaimTypes.Email) ?? string.Empty;
+
+        public UserRole Role => Enum.Parse<UserRole>(
+            _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Role) ?? "Operator");
+
+        public bool IsAuthenticated =>
+            User?.Identity?.IsAuthenticated == true;
+    }
+}
