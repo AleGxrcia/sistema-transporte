a78456eb3c2e19ec15aa9db26df82d146dd231d7
AleGxrcia <fagarciatavarez@gmail.com>
2026-07-09 07:34:44 -0400

    feat(application): add vehicle and driver reassignment command

---
+++ backend/src/TransportSystem.Core.Application/Features/Transportation/Commands/ReassignVehicleAndDriver/ReassignVehicleAndDriverCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace TransportSystem.Core.Application.Features.Transportation.Commands.ReassignVehicleAndDriver
+{
+    public class ReassignVehicleAndDriverCommandValidator : AbstractValidator<ReassignVehicleAndDriverCommand>
+    {
+        public ReassignVehicleAndDriverCommandValidator()
+        {
+            RuleFor(x => x.RequestId).NotEmpty().WithMessage("La solicitud es requerida.");
+            RuleFor(x => x.VehicleId).NotEmpty().WithMessage("El vehículo es requerido.");
+            RuleFor(x => x.DriverId).NotEmpty().WithMessage("El conductor es requerido.");
+        }
+    }
+}
